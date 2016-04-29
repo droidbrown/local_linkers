008d9fbf2ce4897ac710f19a10f3573e8db3c9d2
Prateek Arora <prateekksw@gmail.com>
2016-04-29 13:30:03 +0530

    fixed crash

--- app/src/main/java/com/hbs/hashbrownsys/locallinkers/LocalLinkersApplication.java
+++ app/src/main/java/com/hbs/hashbrownsys/locallinkers/LocalLinkersApplication.java
@@ -2,6 +2,12 @@ package com.hbs.hashbrownsys.locallinkers;
 
 import android.app.Application;
 
+import com.nostra13.universalimageloader.cache.memory.impl.WeakMemoryCache;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
 import com.rollbar.android.Rollbar;
 
 /**
@@ -13,6 +19,25 @@ public class LocalLinkersApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Rollbar.init(this, this.getResources().getString(R.string.rollbar_token), "production");
+
+        // Create global configuration and initialize ImageLoader with this config
+        initImageLoader();
+    }
+
+    public void initImageLoader() {
+        // UNIVERSAL IMAGE LOADER SETUP
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+                .cacheOnDisc(true).cacheInMemory(true)
+                .imageScaleType(ImageScaleType.EXACTLY)
+                .displayer(new FadeInBitmapDisplayer(300)).build();
+
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getApplicationContext())
+                .defaultDisplayImageOptions(defaultOptions)
+                .memoryCache(new WeakMemoryCache())
+                .discCacheSize(100 * 1024 * 1024).build();
+
+        ImageLoader.getInstance().init(config);
+        // END - UNIVERSAL IMAGE LOADER SETUP
     }
 
 }
